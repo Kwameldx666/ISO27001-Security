183ef5937fd9289643e77cf38d4e2bbd3d67e950
Артём Шологан <wonderful_by@bk.ru>
2024-05-30 12:41:43 +0300

    Добавьте файлы проекта.

---
+++ eUseControl.Web/Models/RiskLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web.Models
+{
+    public class RiskLevel
+    {
+        public string Question { get; set; }
+        public string Level { get; set; }
+        public string Answer { get; set; }
+        public int Number { get; set; }
+    }
+}
+
